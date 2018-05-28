b2347443a922d3f8393a931e4f58df23aeadbe81
Alex Deem <alex.deem@tabcorp.com.au>
2018-05-28 14:20:23 +1000

    Performance Optimisation
    Gain an 8% performance improvement in the case
    that the opening command exactly matches the
    closing command tag (ie. case-sensitive match).
    
    This comes at the cost of an 8% performance
    degradation should this not be true.

--- Source/Internal/ScannerState.swift
+++ Source/Internal/ScannerState.swift
@@ -18,8 +18,8 @@ struct ScannerState {
     internal mutating func apply(command:Substring, param:Substring?) {
         var item = stack.last!
 
-        item.command = command.lowercased()
-        switch (item.command) {
+        item.command = command
+        switch (command.lowercased()) {
         case "bold":
             item.style.options.insert(.bold)
         case "italic":
@@ -37,13 +37,15 @@ struct ScannerState {
 
     internal mutating func negate(command:Substring) -> Bool {
         let item = stack.popLast()!
-        return item.command == command.lowercased()
+        // try exact match first, fallback to case-insensitive; this gives a ~8% improvement in the very common-case
+        // that the open and close tags exactly match, at the cost of a ~8% degradation when they don't
+        return item.command == command || item.command?.lowercased() == command.lowercased()
     }
 }
 
 private struct ScannerStateItem {
     var style : EnrichedTextStyle
     var nofill : Bool
-    var command : String?
+    var command : Substring?
 }
 
